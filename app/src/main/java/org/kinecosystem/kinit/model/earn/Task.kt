2d230d3cacd4b6743aa1b6f21cd60fee474fd083
Aryeh Katz <in@akatz.io>
2018-08-13 18:34:56 +0300

    fix deep compare

--- app/src/main/java/org/kinecosystem/kinit/model/earn/Task.kt
+++ app/src/main/java/org/kinecosystem/kinit/model/earn/Task.kt
@@ -10,34 +10,52 @@ const val TASK_TYPE_TRUEX: String = "truex"
 
 
 data class Task(
-    @SerializedName("id")
-    val id: String? = null,
-    @SerializedName("memo")
-    val memo: String? = null,
-    @SerializedName("title")
-    val title: String? = null,
-    @SerializedName("desc")
-    val description: String? = null,
-    @SerializedName("price")
-    val kinReward: Int? = null,
-    @SerializedName("start_date")
-    val startDateInSeconds: Long? = 0,
-    @SerializedName("min_to_complete")
-    val minToComplete: Float? = null,
-    @SerializedName("tags")
-    val tags: List<String>? = null,
-    @SerializedName("provider")
-    val provider: Provider? = null,
-    @SerializedName("type")
-    val type: String? = null,
-    @SerializedName("updated_at")
-    val lastUpdatedAt: Long? = null,
-    @SerializedName("items")
-    val questions: List<Question>? = null)
+        @SerializedName("id")
+        val id: String? = null,
+        @SerializedName("memo")
+        val memo: String? = null,
+        @SerializedName("title")
+        val title: String? = null,
+        @SerializedName("desc")
+        val description: String? = null,
+        @SerializedName("price")
+        val kinReward: Int? = null,
+        @SerializedName("start_date")
+        val startDateInSeconds: Long? = 0,
+        @SerializedName("min_to_complete")
+        val minToComplete: Float? = null,
+        @SerializedName("tags")
+        val tags: List<String>? = null,
+        @SerializedName("provider")
+        val provider: Provider? = null,
+        @SerializedName("type")
+        val type: String? = null,
+        @SerializedName("updated_at")
+        val lastUpdatedAt: Long? = null,
+        @SerializedName("items")
+        val questions: List<Question>? = null) {
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null) return false
+        if (other !is Task) return false
+        if (id != other.id) return false
+        if (memo != other.memo) return false
+        if (title != other.title) return false
+        if (description != other.description) return false
+        if (kinReward != other.kinReward) return false
+        if (minToComplete != other.minToComplete) return false
+        if (tags != other.tags) return false
+        if (provider != other.provider) return false
+        if (type != other.type) return false
+        if (lastUpdatedAt != other.lastUpdatedAt) return false
+        if (questions != other.questions) return false
+        return true
+    }
+}
 
 fun Task.isValid(): Boolean {
     if (id.isNullOrBlank() || title.isNullOrBlank() || description.isNullOrBlank() || type.isNullOrBlank() || memo.isNullOrBlank() ||
-        kinReward == null || minToComplete == null || provider == null || questions == null || startDateInSeconds == null || lastUpdatedAt == null) {
+            kinReward == null || minToComplete == null || provider == null || questions == null || startDateInSeconds == null || lastUpdatedAt == null) {
         return false
     }
     if (isQuiz()) {
